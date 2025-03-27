eb404b2810ac7c4f8807699589725f21e500d257
Rifqi Haikal Chairiansyah <112598058+Rifqi-HaikalCh@users.noreply.github.com>
2025-03-27 16:04:25 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');c
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 // const { Driver } = require('../models');
