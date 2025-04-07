cd6669cffbf3c65a0220d4cd70e80297f1b95043
Hanny Yosephine <hannyyosephine37@gmail.com>
2025-04-08 05:31:28 +0700

    perubahan swagger dan routes customer

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -17,10 +17,10 @@ const {
 const router = express.Router();
 
 // User routes
-router.get('/', verifyToken, isAdmin, getAllCustomers); // Hanya admin
-router.get('/:id', verifyToken, isAdmin, getCustomerById); // Hanya admin
-router.post('/', verifyToken, isAdmin, createCustomerValidator, validate, createCustomer); // Hanya admin
-router.put('/:id', verifyToken, isAdmin, updateCustomerValidator, validate, updateCustomer); // Hanya admin
-router.delete('/:id', verifyToken, isAdmin, deleteCustomerValidator, validate, deleteCustomer); // Hanya admin
+router.get('/', verifyToken(), isAdmin, getAllCustomers); // Hanya admin
+router.get('/:id', verifyToken(), isAdmin, getCustomerById); // Hanya admin
+router.post('/', verifyToken(), isAdmin, createCustomerValidator, validate, createCustomer); // Hanya admin
+router.put('/:id', verifyToken(), isAdmin, updateCustomerValidator, validate, updateCustomer); // Hanya admin
+router.delete('/:id', verifyToken(), isAdmin, deleteCustomerValidator, validate, deleteCustomer); // Hanya admin
 
 module.exports = router;
