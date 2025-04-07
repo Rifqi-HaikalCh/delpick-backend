2d133666143be3651e88a00dd7b11169aa815478
Hanny Yosephine <hannyyosephine37@gmail.com>
2025-04-07 08:36:17 +0700

    remove node modules, .env & logs

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ initSocketIO(server);
 
 // Start the server
 server.listen(PORT, () => {
-    logger.info(`Server running on https://delpick.fun/${PORT}`);
-    logger.info(`Documentation available at https://delpick.fun/${PORT}/api-docs`);
+    logger.info(`Server running on https://delpick.fun/`);
+    logger.info(`Documentation available at https://delpick.fun/api-docs`);
+    // logger.info(`Documentation available at https://delpick.fun/${PORT}/api-docs`);
 }).on('error', (err) => {
     logger.error(`Failed to start server: ${err.message}`);
 });
