a0c121d017716a76344bac1e2dc469e7df12002a
Rifqi Haikal Chairiansyah <112598058+Rifqi-HaikalCh@users.noreply.github.com>
2025-04-08 08:30:07 +0700

    verifyToken

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -20,15 +20,15 @@ const {
 const router = express.Router();
 
 // Admin routes
-router.get('/', verifyToken, isAdmin, getAllDrivers);
-router.get('/:id', verifyToken, isAdmin, getDriverById); // Hanya admin
-router.post('/', verifyToken, isAdmin, createDriverValidator, validate, createDriver); // Hanya admin
-router.put('/:id', verifyToken, isAdmin, updateDriverValidator, validate, updateDriver); // Hanya admin
-router.delete('/:id', verifyToken, isAdmin, deleteDriverValidator, validate, deleteDriver); // Hanya admin
+router.get('/', verifyToken(), isAdmin, getAllDrivers);
+router.get('/:id', verifyToken(), isAdmin, getDriverById); // Hanya admin
+router.post('/', verifyToken(), isAdmin, createDriverValidator, validate, createDriver); // Hanya admin
+router.put('/:id', verifyToken(), isAdmin, updateDriverValidator, validate, updateDriver); // Hanya admin
+router.delete('/:id', verifyToken(), isAdmin, deleteDriverValidator, validate, deleteDriver); // Hanya admin
 
 // Driver routes
-router.put('/location', verifyToken, isDriver, updateDriverLocation); // Hanya driver
-router.put('/status', verifyToken, isDriver, updateDriverStatus); // Hanya driver
-router.put('/updateProfileDriver', verifyToken, isDriver, updateDriverByDriver);
+router.put('/location', verifyToken(), isDriver, updateDriverLocation); // Hanya driver
+router.put('/status', verifyToken(), isDriver, updateDriverStatus); // Hanya driver
+router.put('/updateProfileDriver', verifyToken(), isDriver, updateDriverByDriver);
 
 module.exports = router;
