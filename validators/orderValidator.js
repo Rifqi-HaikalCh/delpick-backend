2e7af5ec2e34eeed24b5e98e0e00fc6c36145876
Rifqi Haikal Chairiansyah <81764285+hannyyosephine16@users.noreply.github.com>
2025-04-09 21:16:35 +0700

    Update orderValidator.js

--- validators/orderValidator.js
+++ validators/orderValidator.js
@@ -4,18 +4,10 @@ const { body, validationResult } = require('express-validator');
  * Validator untuk membuat order baru
  */
 const placeOrderValidator = [
-    body('deliveryAddress').notEmpty().withMessage('Alamat pengiriman harus diisi'),
-    body('subtotal').isFloat({ min: 0 }).withMessage('Subtotal harus berupa angka dan minimal 0'),
-    body('serviceCharge').isFloat({ min: 0 }).withMessage('Biaya layanan harus berupa angka dan minimal 0'),
-    body('total').isFloat({ min: 0 }).withMessage('Total harus berupa angka dan minimal 0'),
-    body('status').notEmpty().withMessage('Status harus diisi'),
-    body('orderDate').isISO8601().withMessage('Format tanggal order tidak valid'),
+    body('storeId').isInt().withMessage('ID toko harus berupa string'),
     body('notes').optional().isString().withMessage('Catatan harus berupa string'),
-    body('driverId').isInt({ min: 1 }).withMessage('ID driver harus berupa angka dan minimal 1'),
-    body('storeId').isString().withMessage('ID toko harus berupa string'),
-    body('items').isArray().withMessage('Items harus berupa array'),
-    body('items.*.name').notEmpty().withMessage('Nama item harus diisi'),
-    body('items.*.price').isFloat({ min: 0 }).withMessage('Harga item harus berupa angka dan minimal 0'),
+    body('items').isArray({ min: 1 }).withMessage('Items harus berupa array dengan minimal 1 item'),
+    body('items.*.itemId').isFloat({ min: 0 }).withMessage('ID item harus berupa angka dan minimal 1'),
     body('items.*.quantity').isInt({ min: 1 }).withMessage('Kuantitas item harus berupa angka dan minimal 1'),
 ];
 
